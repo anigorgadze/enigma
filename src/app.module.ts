fde8b765956ee75d60edddaba6dced8932bfd636
palignusw <firstnamelastname252003@gmail.com>
2024-07-11 15:45:05 +0400

    updated imports in app.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,16 +8,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthorsModule } from './authors/authors.module';
 
 @Module({
-  imports: [MusicsModule, UsersModule,AuthorModule,AlbumsModule, TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username:'root',
-    password: '',
-    database: 'enigma',
-    autoLoadEntities :true,
-    synchronize: true
-  })],
+  imports: [
+    MusicsModule,
+    UsersModule,
+    AuthorsModule,
+    AlbumsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'enigma',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
