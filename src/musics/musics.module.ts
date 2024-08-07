a77448770de5aad2cdd28cb3c65dbd0439cb519d
anigorgadze <anigorgadze30@gmail.com>
2024-08-07 12:37:28 +0400

    fixed FilesModule

--- src/musics/musics.module.ts
+++ src/musics/musics.module.ts
@@ -4,7 +4,6 @@ import { MusicsService } from './musics.service';
 import { MusicsRepository } from './musics.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MusicEntity } from './entities/music.entity';
-import { FilesService } from 'src/files/files.service';
 import { FilesModule } from 'src/files/files.module';
 
 @Module({
