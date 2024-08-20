f66022d7e9c022e3b65b5ed30e08a717c422de8d
anigorgadze <anigorgadze30@gmail.com>
2024-08-20 17:10:17 +0400

    orderBy limit 4

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule , {cors: true});
+  app.enableCors({
+    origin:"*",
+    allowedHeaders:"*"
+  })
 
   app.useGlobalPipes(
     new ValidationPipe({
