e437b5e8cf87897715b615510e0ad1ee96ff2a18
anigorgadze <anigorgadze30@gmail.com>
2024-08-21 18:34:42 +0400

    fixed

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { SearchService } from './search.service';
+import { Public } from 'src/auth/public.decorator';
 
 @Controller('search')
+@Public()
 export class SearchController {
   constructor(private readonly searchService: SearchService) {}
+  
   @Get()
   findAll(@Query('searchField') search: string) {
     return this.searchService.search(search);
