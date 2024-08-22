9f19fb429ed0d54d883e3a4b05bd05ae3ec3a0e0
anigorgadze <anigorgadze30@gmail.com>
2024-08-22 19:14:54 +0400

    logs

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -11,6 +11,11 @@ AWS.config.update({
   region: process.env.AWS_REGION,
 });
 
+console.log('AWS_ACCESS_KEY_ID:', process.env.AWS_ACCESS_KEY_ID);
+console.log('AWS_SECRET_ACCESS_KEY:', process.env.AWS_SECRET_ACCESS_KEY);
+console.log('AWS_REGION:', process.env.AWS_REGION);
+
+
 @Injectable()
 export class FilesService {
   private readonly s3 = new AWS.S3();
