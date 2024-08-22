c316ad267cfd474e916b84978d37d57100a7aa2c
anigorgadze <anigorgadze30@gmail.com>
2024-08-22 19:50:53 +0400

    f

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -5,18 +5,13 @@ import { v4 as uuidv4 } from 'uuid';
 
 dotenv.config();
 
-const crenditials = new AWS.SharedIniFileCredentials({profile: 'engm-bucket'});
 
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION,
-  credentials:crenditials
 });
 
-console.log('AWS_ACCESS_KEY_ID:', process.env.AWS_ACCESS_KEY_ID);
-console.log('AWS_SECRET_ACCESS_KEY:', process.env.AWS_SECRET_ACCESS_KEY);
-console.log('AWS_REGION:', process.env.AWS_REGION);
 
 
 @Injectable()
