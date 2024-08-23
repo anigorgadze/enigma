7f70c3d6d2d446be653da4293751ecbe5c82cc3e
anigorgadze <anigorgadze30@gmail.com>
2024-08-23 17:27:33 +0400

    add is optionals

--- src/albums/dto/create-albums.dto.ts
+++ src/albums/dto/create-albums.dto.ts
@@ -3,6 +3,7 @@ import {
   IsArray,
   IsDateString,
   IsNumber,
+  IsOptional,
   IsString,
   IsUrl,
 } from 'class-validator';
@@ -18,14 +19,17 @@ export class CreateAlbumsDto {
   artistName: string;
 
   @IsArray()
+  @IsOptional()
   @Transform(({ value }) => (Array.isArray(value) ? value : [value]))
   musicsIds: number[];
 
   @IsArray()
+  @IsOptional()
   @Transform(({ value }) => (Array.isArray(value) ? value : [value]))
   authorsIds: number[];
 
   @IsString()
-  coverImgUrl:string
+  @IsOptional()
+  coverImgUrl: string
 }
 
